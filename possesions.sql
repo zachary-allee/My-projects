b8b2da22ebac21145eb1400fa3da6ef3ddebdf17
zachary-allee <77454241+zachary-allee@users.noreply.github.com>
2021-06-04 16:05:54 -0500

    Update possesions.sql

--- possesions.sql
+++ possesions.sql
@@ -1,5 +1,5 @@
 
--- I found everything I needed in the game table and created a new table with just the values I need right now.
+-- Everything needed for the calculations
   
  CREATE TABLE possesion_analysis AS
 	SELECT 
